60c84e4ef03a94b06aa9ba7ef572e486263b9fbd
Margaretha Olivia <margarethaolivia41@gmail.com>
2024-02-19 18:06:35 +0700

    setup react express

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Define routes or middleware as needed
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
