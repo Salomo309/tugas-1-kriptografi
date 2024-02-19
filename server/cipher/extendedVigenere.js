68d6ce1ac0a4ff094b6430fedb88ee6daf98c8d8
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2024-02-19 18:32:53 +0700

    feat: extended vigenere

---
+++ server/cipher/extendedVigenere.js
@@ -0,0 +1,37 @@
+// encrypting function
+function encrypt(plaintext, key) {
+    let ciphertext = '';
+    for (let i = 0, j = 0; i < plainText.length; i++) {
+        let char = plainText.charCodeAt(i);
+        let keyCharCode = key.charCodeAt(j % key.length);
+
+        let encryptedCharCode = (char + keyChar) % 256;
+        ciphertext += String.fromCharCode(encryptedCharCode);
+
+        j++;
+    }
+    return ciphertext;
+}
+
+// decrypting function
+function decrypt(msg, key) {
+    let plaintext = '';
+    for (let i = 0, j = 0; i < encryptedText.length; i++) {
+        let char = encryptedText.charCodeAt(i);
+        let keyChar = key.charCodeAt(j % key.length);
+
+        let decryptedCharCode = (char - keyChar + 256) % 256;
+        plaintext += String.fromCharCode(decryptedCharCode);
+
+        j++;
+    }
+    return plaintext;
+}
+
+let plainText = "Hello World!";
+let key = "key";
+
+let encryptedText = encrypt(plainText, key);
+console.log("Teks terenkripsi:", encryptedText);
+let decryptedText = decrypt(encryptedText, key);
+console.log("Teks terdekripsi:", decryptedText);
